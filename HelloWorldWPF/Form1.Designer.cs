d0af5e2a686eb4b4418ba77fabb8594f25b662bf
1Fenixxx <game.masterGM71@gmail.com>
2021-02-04 12:00:55 +0200

    WinForms

---
+++ HelloWorldWPF/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace HelloWorldWPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloWorldButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HelloWorldButton
+            // 
+            this.HelloWorldButton.Location = new System.Drawing.Point(12, 12);
+            this.HelloWorldButton.Name = "HelloWorldButton";
+            this.HelloWorldButton.Size = new System.Drawing.Size(157, 50);
+            this.HelloWorldButton.TabIndex = 0;
+            this.HelloWorldButton.Text = "press me";
+            this.HelloWorldButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 74);
+            this.Controls.Add(this.HelloWorldButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HelloWorldButton;
+    }
+}
+
---
+++ HelloWorldWPF/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace HelloWorldWPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HelloWorldButton.Click += HelloWorldButton_Click;
+        }
+
+        private void HelloWorldButton_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("HELLO WORLD");
+        }
+    }
+}
