8806eee93b342cef41a1d58d2b7f046ae6ce4d4f
1Fenixxx <game.masterGM71@gmail.com>
2021-02-04 12:00:39 +0200

    Console

---
+++ firstLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstLab
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            foreach( string str in args)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Hello World, i think");
+
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
